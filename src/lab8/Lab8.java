5b75bc96fcf4ae76506997d218c2e918c1354771
Jerzy9 <zbylutkonrad@gmail.com>
2020-12-12 15:58:20 +0100

    lab 8 init

---
+++ src/lab8/Lab8.java
@@ -0,0 +1,70 @@
+package lab8;
+
+public class Lab8 {
+
+    public Lab8() {
+        //Zadanie 1
+        zad1();
+        //Zadanie 2
+//        zad2();
+        //Zadanie 3
+//        zad3();
+        //Zadanie 4
+//        zad4();
+        //Zadanie 5
+//        zad5();
+        //Zadanie 6
+//        zad6();
+        //Zadanie 7
+//        zad7();
+        //Zadanie 8
+//        zad8();
+        //Zadanie 9
+//        zad9();
+        //Zadanie 10
+//        zad10();
+
+    }
+
+    public void zad1() {
+
+    }
+
+    public void zad2() {
+
+    }
+
+    public void zad3() {
+
+    }
+
+    public void zad4() {
+
+    }
+
+    public void zad5() {
+
+    }
+
+    public void zad6() {
+
+    }
+
+    public void zad7() {
+
+    }
+
+    public void zad8() {
+
+    }
+
+    public void zad9() {
+
+    }
+
+    public void zad10() {
+
+    }
+
+
+}
